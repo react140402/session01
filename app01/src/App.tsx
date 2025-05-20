fc7ac26e86b7249f12eaba97251246c832c264e0
Pashaie <pashaie@gmail.com>
2025-05-20 19:21:08 +0330

    app01/src/App: state v1

--- app01/src/App.tsx
+++ app01/src/App.tsx
@@ -9,13 +9,38 @@
  * 
  */
 
+//use* - hook
+import { useState } from "react";
+
 function App() {
+  console.log("App...")
   const msg = "Salam Donya";
+
+  //[number, React.Dispatch<React.SetStateAction<number>>]
+  const counter = useState(0)
+
+
+
+  function inc() {
+    console.log('current:', counter[0])
+    counter[1](counter[0] + 1);
+    console.log(counter[0])
+  }
+
+  function dec() {
+    console.log('current:', counter[0])
+    counter[1](counter[0] - 1);
+    console.log(counter[0])
+  }
 
   return (
     <>
       <div className="container">
         <h1>{msg}</h1>
+        {counter[0]}
+        <br />
+        <button onClick={inc}>➕</button>&nbsp;
+        <button onClick={dec}>➖</button>
       </div>
     </>
   )
